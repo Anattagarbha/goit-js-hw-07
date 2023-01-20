935d6e5ea7a04749621b4dd6db2b725928427fdd
Anattagarbha <ruslanvic@gmail.com>
2023-01-20 14:28:14 +0200

    init vars

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// declare vars
+
+const imgGallery = document.querySelector(".gallery");
+const galleryMarkup = createMarkup(galleryItems);
