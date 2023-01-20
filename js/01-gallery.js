66e4e89ac4e3703209478061ab6bc679c31641bd
Anattagarbha <ruslanvic@gmail.com>
2023-01-20 14:54:34 +0200

    insert markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,6 @@ console.log(galleryItems);
 // init vars
 
 const imgGallery = document.querySelector(".gallery");
-const galleryMarkup = createMarkup(galleryItems);
 
 // declare iterating function
 
@@ -15,8 +14,13 @@ function createMarkup(items) {
     .map(
       (
         item
-      ) => `div class="gallery__item"><a class="gallery__link" href="${item.original}"
+      ) => `<div class="gallery__item"><a class="gallery__link" href="${item.original}">
     <img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a></div>`
     )
     .join("");
 }
+
+// insert markup to DOM
+
+const galleryMarkup = createMarkup(galleryItems);
+imgGallery.innerHTML = galleryMarkup;
