c29d2a29ade292639c3b13919644775e8219b195
Anattagarbha <ruslanvic@gmail.com>
2023-01-21 13:30:33 +0200

    write script using simplelightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// create markup
+
+function createMarkup(items) {
+	return items
+		.map(
+			item =>
+				`<li class="gallery__item">
+					<a class="gallery__link" href="${item.original}">
+					<img 
+					class="gallery__image"
+					src="${item.preview}"
+					alt="${item.description}">
+				</a>
+				</li>`,
+		)
+		.join('');
+}
+
+// init var
+
+const imgGallery = document.querySelector('.gallery');
+
+// insert markup into DOM
+
+imgGallery.innerHTML = createMarkup(galleryItems);
+
+// use lighbox functionality
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+	captionPosition: 'bottom',
+});
