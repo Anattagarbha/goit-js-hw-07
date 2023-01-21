6fc8f06a093a73e24c7fec6ba6b926f50751e1db
Anattagarbha <ruslanvic@gmail.com>
2023-01-21 13:17:04 +0200

    add prettier file

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,23 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
 
 // init vars
 
-const imgGallery = document.querySelector(".gallery");
+const imgGallery = document.querySelector('.gallery');
 const galleryMarkup = createMarkup(galleryItems);
 
 // declare iterating function
 
 function createMarkup(items) {
-  return items
-    .map(
-      (
-        item
-      ) => `<div class="gallery__item"><a class="gallery__link" href="${item.original}">
+	return items
+		.map(
+			item => `<div class="gallery__item"><a class="gallery__link" href="${item.original}">
     <img class="gallery__image" src="${item.preview}" data-source="${item.original}" 
-    alt="${item.description}" /></a></div>`
-    )
-    .join("");
+    alt="${item.description}" /></a></div>`,
+		)
+		.join('');
 }
 
 // insert markup to DOM
@@ -28,36 +26,36 @@ imgGallery.innerHTML = galleryMarkup;
 
 // add listener
 
-imgGallery.addEventListener("click", handleClick);
+imgGallery.addEventListener('click', handleClick);
 
 // use basicLightbox functionality
 
 let instance;
 
 function handleClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  instance = basicLightbox.create(
-    `<div class=""><img src=${event.target.dataset.source} width="1024" height="768"></div>`,
-    {
-      className: "modal",
-      onShow: () => {
-        window.addEventListener("keydown", onEscDown);
-      },
-      onClose: () => {
-        window.removeEventListener("keydown", onEscDown);
-      },
-    }
-  );
-  instance.show();
+	event.preventDefault();
+	if (event.target.nodeName !== 'IMG') {
+		return;
+	}
+	instance = basicLightbox.create(
+		`<div class=""><img src=${event.target.dataset.source} width="1024" height="768"></div>`,
+		{
+			className: 'modal',
+			onShow: () => {
+				window.addEventListener('keydown', onEscDown);
+			},
+			onClose: () => {
+				window.removeEventListener('keydown', onEscDown);
+			},
+		},
+	);
+	instance.show();
 }
 
 // add escape extra functionality
 
 function onEscDown(event) {
-  if (event.code === "Escape") {
-    instance.close();
-  }
+	if (event.code === 'Escape') {
+		instance.close();
+	}
 }
